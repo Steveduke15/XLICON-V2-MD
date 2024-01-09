77d1f5b41dd844c555b6eda64d08ea606f33c0fa
Steveduke15 <145169365+Steveduke15@users.noreply.github.com>
2024-01-09 19:33:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,17 +7,17 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['233533763772', 'Abraham Dwamena', true],
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
-  ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true], 
-  ['918360234087', 'WHITE', true], 
+  ['2348024031146', 'King Venom', true],
+  ['2348024031146', 'Yahoo Boss -(Venom)', true], 
+  ['2348024031146', '𝙼r Venom⚡', true], 
+  ['2348024031146', 'Scorpion King', true], 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['233533763772','923184070915', '94789481495', '923184474176'] 
-global.prems = ['923184070915', '233533763772', '94789481495', '918360234087']
-global.allowed = ['923184070915', '233533763772', '94789481495', '918360234087']
+global.mods = ['2348024031146','2348024031146', '94789481495', '923184474176'] 
+global.prems = ['2348024031146', '2348024031146', '94789481495', '918360234087']
+global.allowed = ['2348024031146', '2348024031146', '94789481495', '918360234087']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -52,10 +52,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'XLICON-V2'
+global.botname = 'Venom.Md-V2'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
-global.author = '@XLICON-V2' 
+global.packname = 'Venom TEAM' 
+global.author = '@Veno.-V2' 
 global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
 global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
 global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
@@ -66,7 +66,7 @@ global.fglog = 'XLICON.jpg'
 global.thumb = fs.readFileSync('./XLICON.jpg')
 
 
-global.wait = '*🕣 _XLICON IS LODIND..._*\n*▰▰▰▱▱▱▱▱⭐*'
+global.wait = '*🕣 _Venom IS LODIND..._*\n*▰▰▰▱▱▱▱▱⭐*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✔️'
